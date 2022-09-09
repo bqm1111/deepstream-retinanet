90aa0fcb153eebee1f004b5185b5b50a4e5d81b1
bqm1111 <buiquangminh1111@gmail.com>
2022-09-09 10:25:10 +0700

    Fixed yolo

---
+++ src/faceApp.h
@@ -0,0 +1,25 @@
+#ifndef APP_H
+#define APP_H
+#include "PipelineHandler.h"
+#include <gst/gstelement.h>
+#include <gst/gstpipeline.h>
+#include <string>
+
+class FaceApp
+{
+public:
+    FaceApp(std::string name);
+    ~FaceApp();
+
+    std::vector<std::string> m_video_source_path;
+    GstAppParam m_gstparam;
+    AppPipeline m_app;
+    std::string m_app_name;
+    void add_video(std::string video_path, std::string video_name);
+    void linkMuxer();
+    void showVideo();
+    void faceDetection();
+    void detectAndSend();
+    GstElement * getPipeline();
+};
+#endif
